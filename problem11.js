c01cd409631491af40f290e54653ba5bd4552731
kuradesuraj999 <kuradesuraj999@gmail.com>
2022-09-15 23:35:55 +0530

    correct the code

---
+++ problem11.js
@@ -0,0 +1,21 @@
+let N=6;
+    let str="nrupul";
+ 
+   bag="";
+  
+  for(let i=N-1;i>=0;i--){
+
+
+    
+      bag=bag+str[i];
+   
+ }
+   if (bag=!str){
+  
+     console.log("Yes");
+
+   }else{
+   
+    console.log("No")
+ 
+  }
---
+++ problem22.js
@@ -0,0 +1,22 @@
+count=0;
+  
+  for(let a=1;a<=num;a++){
+ 
+       if(num%a==0){
+     
+        count++;
+      
+      }
+         
+   }if(count>2){
+   
+             console.log("No");
+ 
+           }
+      
+      else
+            {
+ 
+               console.log("Yes");
+    
+        }
